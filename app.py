97c5d307e8da228bc6d4110e3595b5634c2bd21b
bishal <bishalbhaandari62@gmail.com>
2025-12-12 10:34:28 +0100

    Adding error handling-400

--- app.py
+++ app.py
@@ -119,6 +119,9 @@ def delete_drink(id):
 @app.errorhandler(404)
 def not_found(e):
     return jsonify({"error": "Resource not found"}),404
+@app.errorhandler(400)
+def bad_request(e):
+    return jsonify({"error": "Bad request"}), 400
 
 
 # TERMINAL COMMAND FUNCTIONS
